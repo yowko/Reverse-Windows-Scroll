d59579ccfc01da47868c45052a16cd77ca6e1b57
Yowko Tsai <yowko0729@gmail.com>
2019-02-04 00:36:28 +0800

    add powershell file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Reverse-Windows-Scroll
+
+## 取得 Device instance path
+1. open Device Manager
+
+    ![2devicemanager](https://user-images.githubusercontent.com/3851540/52179357-88bc2780-2814-11e9-985b-45a6f4592d56.png)
+
+2. Right-click on target device --> Properties
+
+    ![3properties](https://user-images.githubusercontent.com/3851540/52179358-88bc2780-2814-11e9-8ae4-42d5b514e6b6.png)
+
+3. Property --> Device instance path --> Copy
+
+    ![4devicepath](https://user-images.githubusercontent.com/3851540/52179359-88bc2780-2814-11e9-80c6-bca33901bc1a.png)
---
+++ windows-reverse-scroll.ps1
@@ -0,0 +1,11 @@
+﻿# 提示輸入 device instance path (input device instance path)
+    $deviceInstancePath=Read-Host -Prompt 'Input your device instnace path'
+    # 指定 registry 位置 (set registry path)
+    $RegKey="HKLM:\SYSTEM\CurrentControlSet\Enum\"+$deviceInstancePath+"\Device Parameters"
+
+    # 顯示目前設定值(show current setting)
+    Get-ItemProperty -path $regKey -Name FlipFlopWheel
+    # 設定捲軸反轉(set reverse scrll)
+    Set-ItemProperty -path $regKey -Name FlipFlopWheel -Value 1
+    # 確認設定結果 (confirm new setting)
+    Get-ItemProperty -path $regKey -Name FlipFlopWheel
